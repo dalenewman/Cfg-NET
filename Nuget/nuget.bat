ff388d65e805400ee53dc906a36044c1d6053ff4
Dale Newman <dalenewman@gmail.com>
2018-09-27 22:14:12 -0400

    updated nuget script

--- Nuget/nuget.bat
+++ Nuget/nuget.bat
@@ -2,3 +2,8 @@ nuget.exe pack Cfg.Net.nuspec -OutputDirectory "c:\temp\modules"
 nuget.exe pack Cfg.Net.Environment.nuspec -OutputDirectory "c:\temp\modules"
 nuget.exe pack Cfg.Net.Reader.nuspec -OutputDirectory "c:\temp\modules"
 nuget.exe pack Cfg.Net.Shorthand.nuspec -OutputDirectory "c:\temp\modules"
+
+nuget.exe push "c:\temp\modules\Cfg-NET.0.10.4.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget.exe push "c:\temp\modules\Cfg-NET.Reader.0.3.3.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget.exe push "c:\temp\modules\Cfg-NET.Environment.0.3.2.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget.exe push "c:\temp\modules\Cfg-NET.Shorthand.0.2.11.nupkg" -source https://api.nuget.org/v3/index.json
