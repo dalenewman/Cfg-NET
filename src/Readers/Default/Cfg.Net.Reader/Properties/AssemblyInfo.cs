6e9186d52f6adca4479276517144de0e00106bf8
Dale Newman <dalenewman@gmail.com>
2019-11-05 10:00:26 -0500

    Fix nuget stuff for reader

--- src/Readers/Default/Cfg.Net.Reader/Properties/AssemblyInfo.cs
+++ src/Readers/Default/Cfg.Net.Reader/Properties/AssemblyInfo.cs
@@ -25,7 +25,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dale Newman")]
 [assembly: AssemblyProduct("Cfg-Net")]
-[assembly: AssemblyCopyright("Copyright ©  2015-2017")]
+[assembly: AssemblyCopyright("Copyright © 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,5 +47,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.3.0")]
-[assembly: AssemblyFileVersion("0.3.3.0")]
+[assembly: AssemblyVersion("0.3.4.0")]
+[assembly: AssemblyFileVersion("0.3.4.0")]
