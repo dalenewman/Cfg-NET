5841b2fb6d29bcfe316cf69ba18c09c2a69b9196
Dale Newman <dalenewman@gmail.com>
2019-11-05 13:23:15 -0500

    Added .net standard 2.0 targets for customizers

--- Nuget/nuget.bat
+++ Nuget/nuget.bat
@@ -5,5 +5,5 @@ nuget.exe pack Cfg.Net.Shorthand.nuspec -OutputDirectory "c:\temp\modules"
 
 REM nuget.exe push "c:\temp\modules\Cfg-NET.0.10.5.nupkg" -source https://api.nuget.org/v3/index.json
 REM nuget.exe push "c:\temp\modules\Cfg-NET.Reader.0.3.4.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget.exe push "c:\temp\modules\Cfg-NET.Environment.0.3.4.nupkg" -source https://api.nuget.org/v3/index.json
-REM nuget.exe push "c:\temp\modules\Cfg-NET.Shorthand.0.2.11.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget.exe push "c:\temp\modules\Cfg-NET.Environment.0.3.5.nupkg" -source https://api.nuget.org/v3/index.json
+REM nuget.exe push "c:\temp\modules\Cfg-NET.Shorthand.0.2.12.nupkg" -source https://api.nuget.org/v3/index.json
